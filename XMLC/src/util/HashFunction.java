d704706a77e74502b957567c122141b2e455ddec
Karlson Pfannschmidt <kiudee@mail.upb.de>
2015-11-05 10:23:53 +0100

    Add murmurhash with wrapper

---
+++ XMLC/src/util/HashFunction.java
@@ -0,0 +1,24 @@
+package util;
+
+public class HashFunction {
+	private Murmur3A murmur;
+	private int period;
+	private boolean isSign;
+
+	public HashFunction(int seed, int period) {
+		this.murmur = new Murmur3A(seed);
+		this.period = period;
+		this.isSign = false;
+	}
+	
+	public HashFunction(int seed) {
+		this.murmur = new Murmur3A(seed);
+		this.isSign = true;
+	}
+	
+	public int hash(int index) {
+		this.murmur.update(index);
+		if (this.isSign) return (int) (this.murmur.getValue() % 2 * 2 - 1);
+		return (int) (this.murmur.getValue() % this.period);
+	}
+}
