c25227a3a4251df5ef90e87486bd0af2680e4a4a
Karlson Pfannschmidt <kiudee@mail.upb.de>
2015-11-24 11:16:16 +0100

    Define StepFunction interface

---
+++ XMLC/src/Learner/StepFunction.java
@@ -0,0 +1,20 @@
+package Learner;
+
+import jsat.linear.Vec;
+
+/**
+ *
+ * @author Karlson Pfannschmidt
+ */
+public interface StepFunction {
+	/**
+	 * Applies a gradient descent step to the weight vector {@code w}.
+	 * @param w weight vector
+	 * @param grad vector of gradients
+	 * @param bias
+	 * @param biasGrad
+	 * @return adjustment to bias:
+	 * {@code bias = bias - returnValue}
+	 */
+	public abstract double step(Vec w, Vec grad, double bias, double biasGrad);
+}
